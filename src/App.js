c2c436fa63d0593feef567271c79800f30d45c70
pauline-wim <linette.wim@hotmail.com>
2022-01-31 13:07:49 +0100

    validEmail && validPassword

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ class App extends React.Component {
     this.state = {
       emailValue: "",
       passwordValue: "",
+      validEmail: "is-invalid",
+      validPassword: "is-invalid",
     };
 
     this.emailChange = this.emailChange.bind(this);
@@ -20,10 +22,20 @@ class App extends React.Component {
 
   emailChange(e) {
     this.setState({ emailValue: e.target.value });
+    if (
+      this.state.emailValue.match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      )
+    ) {
+      this.setState({ validEmail: "is-valid" });
+    }
   }
 
   passwordChange(e) {
     this.setState({ passwordValue: e.target.value });
+    if (this.state.passwordValue.length > 5) {
+      this.setState({ validPassword: "is-valid" });
+    }
   }
 
   handleSubmit(e) {
@@ -35,6 +47,8 @@ class App extends React.Component {
     return (
       <div>
         <Form
+          validEmail={this.state.validEmail}
+          validPassword={this.state.validPassword}
           onSubmit={this.handleSubmit}
           emailValue={this.state.emailValue}
           passwordValue={this.state.passwordValue}
--- src/components/Form.js
+++ src/components/Form.js
@@ -3,13 +3,26 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 class Form extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
   render() {
     return (
-      <form action="" onSubmit={this.props.onSubmit}>
+      <form
+        onSubmit={this.props.onSubmit}
+        className="needs-validation"
+        novalidate
+      >
         <h1>Login</h1>
-        <div>
-          <label htmlFor="email">Email address</label>
+        <div className="mb-3">
+          <label className="form-label" htmlFor="email">
+            Email address
+          </label>
           <input
+            className={`form-control ${this.props.validEmail}`}
             type="email"
             name="email"
             id="email"
@@ -19,9 +32,12 @@ class Form extends React.Component {
             required
           />
         </div>
-        <div>
-          <label htmlFor="password">Password</label>
+        <div className="mb-3">
+          <label className="form-label" htmlFor="password">
+            Password
+          </label>
           <input
+            className={`form-control ${this.props.validPassword}`}
             type="password"
             name="password"
             id="password"
@@ -31,11 +47,20 @@ class Form extends React.Component {
             required
           />
         </div>
-        <div>
-          <input type="checkbox" name="rememberMe" id="rememberMe" />
-          <label for="rememberMe">Remember me</label>
+        <div className="mb-3 form-check">
+          <input
+            className="form-check-input"
+            type="checkbox"
+            name="rememberMe"
+            id="rememberMe"
+          />
+          <label className="form-check-label" for="rememberMe">
+            Remember me
+          </label>
         </div>
-        <button type="submit">Submit</button>
+        <button className="btn btn-primary" type="submit">
+          Submit
+        </button>
       </form>
     );
   }
