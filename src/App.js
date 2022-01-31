8aca2eb140607c7b3c211053c39fd20d485aa566
pauline-wim <linette.wim@hotmail.com>
2022-01-31 14:47:58 +0100

    working Form with CSS

--- src/App.js
+++ src/App.js
@@ -13,6 +13,8 @@ class App extends React.Component {
       passwordValue: "",
       validEmail: "is-invalid",
       validPassword: "is-invalid",
+      displayForm: "d-block",
+      displaySubmit: "d-none",
     };
 
     this.emailChange = this.emailChange.bind(this);
@@ -24,7 +26,7 @@ class App extends React.Component {
     this.setState({ emailValue: e.target.value });
     if (
       this.state.emailValue.match(
-        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{1,}))$/
       )
     ) {
       this.setState({ validEmail: "is-valid" });
@@ -33,29 +35,40 @@ class App extends React.Component {
 
   passwordChange(e) {
     this.setState({ passwordValue: e.target.value });
-    if (this.state.passwordValue.length > 5) {
+    if (
+      this.state.passwordValue.match(
+        // Minimum six characters (letters), max 2O and at least one number:
+        /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{5,20}$/
+      )
+    ) {
       this.setState({ validPassword: "is-valid" });
     }
   }
 
   handleSubmit(e) {
-    alert("Form submitted");
+    // alert("Form submitted");
     e.preventDefault();
+    {
+      this.setState({ displayForm: "d-none" });
+      this.setState({ displaySubmit: "d-flex" });
+    }
   }
 
   render() {
     return (
-      <div>
+      <section className="m-4 p-4 d-flex justify-content-center">
         <Form
+          onSubmit={this.handleSubmit}
           validEmail={this.state.validEmail}
           validPassword={this.state.validPassword}
-          onSubmit={this.handleSubmit}
           emailValue={this.state.emailValue}
           passwordValue={this.state.passwordValue}
           emailChange={this.emailChange}
           passwordChange={this.passwordChange}
+          displayForm={this.state.displayForm}
+          displaySubmit={this.state.displaySubmit}
         />
-      </div>
+      </section>
     );
   }
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -11,57 +11,68 @@ class Form extends React.Component {
 
   render() {
     return (
-      <form
+      <div
         onSubmit={this.props.onSubmit}
-        className="needs-validation"
+        className="needs-validation w-50 p-4"
         novalidate
       >
-        <h1>Login</h1>
-        <div className="mb-3">
-          <label className="form-label" htmlFor="email">
-            Email address
-          </label>
-          <input
-            className={`form-control ${this.props.validEmail}`}
-            type="email"
-            name="email"
-            id="email"
-            placeholder="Enter email..."
-            value={this.props.emailValue}
-            onChange={this.props.emailChange}
-            required
-          />
+        <h1 className="text-center">Login</h1>
+
+        {/* Fill out form */}
+        <form className={`p-4 ${this.props.displayForm}`}>
+          <div className="mb-3">
+            <label className="form-label" htmlFor="email">
+              Email address
+            </label>
+            <input
+              className={`form-control ${this.props.validEmail}`}
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Enter email..."
+              value={this.props.emailValue}
+              onChange={this.props.emailChange}
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <label className="form-label" htmlFor="password">
+              Password
+            </label>
+            <input
+              className={`form-control ${this.props.validPassword}`}
+              type="password"
+              name="password"
+              id="password"
+              placeholder="Enter password..."
+              value={this.props.passwordValue}
+              onChange={this.props.passwordChange}
+              required
+            />
+          </div>
+          <div className="mb-3 form-check">
+            <input
+              className="form-check-input"
+              type="checkbox"
+              name="rememberMe"
+              id="rememberMe"
+            />
+            <label className="form-check-label" for="rememberMe">
+              Remember me
+            </label>
+          </div>
+          <button className="btn btn-primary" type="submit">
+            Submit
+          </button>
+        </form>
+
+        {/* Display after submit */}
+        <div
+          className={`m-3 p-4 h-100 justify-content-center align-items-center bg-light rounded-3 ${this.props.displaySubmit}`}
+        >
+          <h1>Form submitted</h1>
         </div>
-        <div className="mb-3">
-          <label className="form-label" htmlFor="password">
-            Password
-          </label>
-          <input
-            className={`form-control ${this.props.validPassword}`}
-            type="password"
-            name="password"
-            id="password"
-            placeholder="Enter password..."
-            value={this.props.passwordValue}
-            onChange={this.props.passwordChange}
-            required
-          />
-        </div>
-        <div className="mb-3 form-check">
-          <input
-            className="form-check-input"
-            type="checkbox"
-            name="rememberMe"
-            id="rememberMe"
-          />
-          <label className="form-check-label" for="rememberMe">
-            Remember me
-          </label>
-        </div>
-        <button className="btn btn-primary" type="submit">
-          Submit
-        </button>
-      </form>
+      </div>
     );
   }
 }
